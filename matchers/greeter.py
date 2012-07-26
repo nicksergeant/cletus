b94630e7aae6695038f2599e4b97e7fc00f17675
Nick Sergeant <nick@nicksergeant.com>
2012-07-26 12:48:21 -0400

    Cletusy things

--- matchers/greeter.py
+++ matchers/greeter.py
@@ -16,24 +16,57 @@ class GreetingMatcher(BaseMatcher):
         'hi',
         'hey',
         'hello',
-        'sup',
         'howdy',
     ]
-
-    responses = [
-        'how y\'all doin',
-        'yeeeaasiree',
-        'sup, yankee',
-        'howdy tater',
-        'how do?',
-        'what\'s good?',
-        'yeap',
-        'yeallow',
+    leavings = [
+        'leaving',
+        'done for the day',
+        'stopping for the day',
+        'stopping here',
     ]
 
     def respond(self, message, user=None):
         if message.lower() in self.greetings:
-            message = random.choice(self.responses)
+            responses = [
+                'how y\'all doin',
+                'yeeeaasiree',
+                'sup, yankee',
+                'howdy tater',
+                'how do?',
+                'what\'s good?',
+                'yeap',
+                'yeallow',
+            ]
+            message = random.choice(responses)
+            if user:
+                message = user + ": " + message
+            self.speak(message)
+
+        if message.lower() in self.leavings:
+            laters = [
+                'catch ya on the flipside',
+                'later varmint',
+                'skedaddle',
+                'y\'all come back now, ya-hear',
+            ]
+            message = random.choice(laters)
+            if user:
+                message = user + ": " + message
+            self.speak(message)
+
+        if 'sup cletus' in message.lower():
+            sups = [
+                'Them fellas done killed a 9 point yesterday morn',
+                'To get to their house, go up the holler, follow the creck, past to old oak and turn north',
+                'If you want to get something done, find a busy man. There\'s a reason he\'s busy',
+            ]
+            message = random.choice(sups)
+            if user:
+                message = user + ": " + message
+            self.speak(message)
+
+        if 'how long' in message.lower():
+            message = 'Ima figurin\' it\'ll be two days'
             if user:
                 message = user + ": " + message
             self.speak(message)
